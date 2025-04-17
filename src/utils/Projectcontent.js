6b9015e05efa54aba52ca584a780471a81e13864
Ravi Shankar Singh <95556757+ravisingh9302@users.noreply.github.com>
2025-04-17 23:42:16 +0530

    Update Projectcontent.js

--- src/utils/Projectcontent.js
+++ src/utils/Projectcontent.js
@@ -2,7 +2,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-chatapp.vercel.app/login",
       sourcecode:"https://github.com/ravisingh9302/chatapp",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_435_d76ten.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_435_d76ten.png",
       title: "Chat Application",
       description:
         "A real-time chat application built with React, Express, MongoDB, and Socket.io, featuring peer-to-peer messaging, online status, typing indicators, and emoji support. The app leverages WebSocket for instant updates and provides a seamless communication experience.",
@@ -10,7 +10,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.vercel.app/",
       sourcecode:"https://www.npmjs.com/package/iframewidget",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1719059483/Screenshot_453_wvenjp.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1719059483/portfolio/Screenshot_453_wvenjp.png",
       title: "Widget Npm Package",
       description:
         "A versatile widget npm package built with Node.js and React, designed for easy integration into any webpage. It allows full customization to match your site's theme and can host your website or chatbot, providing a seamless and personalized user experience.",
@@ -18,7 +18,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/widget",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624867/Screenshot_450_xcnzxn.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624867/portfolio/Screenshot_450_xcnzxn.png",
       title: "Telegram Chat Widget",
       description:
         "A Telegram messenger and webpage widget bot created with Vite, Socket.io, Telegram Bot, and Express, enabling live customer support. Website visitors can chat directly with support agents, get their queries answered in real-time, and experience seamless communication between the webpage and Telegram.",
@@ -26,7 +26,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-screensharing.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/screensharing",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/Screenshot_441_ppyovg.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/portfolio/Screenshot_441_ppyovg.png",
       title: "Screen Sharing Application",
       description:
         "A screen sharing application built with React, Express, Socket.io, and WebRTC, featuring peer-to-peer screen sharing without a central server. Users can toggle camera and mic on/off, share their screen with a webcam overlay, and join calls using a unique ID for a seamless and interactive experience."
@@ -34,7 +34,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-videocalling.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/videocalling",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624872/Screenshot_443_f1c7gm.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624872/portfolio/Screenshot_443_f1c7gm.png",
       title: "Video Calling Application",
       description:
         "A video calling application built with React, Express, Socket.io, and WebRTC, featuring peer-to-peer video calls without a central server. Users can join calls with a unique ID, and have control over camera and microphone settings with on/off features for a seamless communication experience.",
@@ -42,7 +42,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/invoice_generator/",
       sourcecode:"https://github.com/ravisingh9302/invoice_generator",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/Screenshot_434_qbb04x.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/portfolio/Screenshot_434_qbb04x.png",
       title: "Invoice Generator",
       description:
         "An invoice generator application built with React, featuring comprehensive item entry, discount management, currency settings, and GST handling. This tool simplifies invoice creation with additional functionalities for detailed and customized billing.",
@@ -60,7 +60,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-chatapp.vercel.app/login",
       sourcecode:"https://github.com/ravisingh9302/chatapp",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_435_d76ten.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_435_d76ten.png",
       title: "Chat Application",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -68,7 +68,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.vercel.app/",
       sourcecode:"https://www.npmjs.com/package/iframewidget",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1719059483/Screenshot_453_wvenjp.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1719059483/portfolio/Screenshot_453_wvenjp.png",
       title: "Widget Npm package",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -76,7 +76,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/widget",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624867/Screenshot_450_xcnzxn.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624867/portfolio/Screenshot_450_xcnzxn.png",
       title: "Telegram Chat Widget",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -84,7 +84,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-screensharing.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/screensharing",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/Screenshot_441_ppyovg.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/portfolio/Screenshot_441_ppyovg.png",
       title: "Screen Sharing Application",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -92,7 +92,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh-videocalling.vercel.app/",
       sourcecode:"https://github.com/ravisingh9302/videocalling",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624872/Screenshot_443_f1c7gm.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624872/portfolio/Screenshot_443_f1c7gm.png",
       title: "Video Calling Application",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -101,7 +101,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/invoice_generator/",
       sourcecode:"https://github.com/ravisingh9302/invoice_generator",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/Screenshot_434_qbb04x.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624868/portfolio/Screenshot_434_qbb04x.png",
       title: "Invoice Generator",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -110,7 +110,7 @@ export const ImpProjects = [
     {
       preview:"https://t.me/imagemodify_bot",
       sourcecode:"https://github.com/ravisingh9302/telegram_bot_imagemodify",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_452_a7yq46.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_452_a7yq46.png",
       title: "ImageInfo Telegram Bot",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -118,7 +118,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/snakegame/",
       sourcecode:"https://github.com/ravisingh9302/snakegame",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624873/Screenshot_424_c1fu4d.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624873/portfolio/Screenshot_424_c1fu4d.png",
       title: "Snake Game",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -126,7 +126,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/piano/",
       sourcecode:"https://github.com/ravisingh9302/piano",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_431_kafh29.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_431_kafh29.png",
       title: "Piano ",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -134,7 +134,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/clock/",
       sourcecode:"https://github.com/ravisingh9302/clock",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_425_pazzwq.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_425_pazzwq.png",
       title: "Clock",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
@@ -142,7 +142,7 @@ export const ImpProjects = [
     {
       preview:"https://ravisingh9302.github.io/knowyourscreen/",
       sourcecode:"https://github.com/ravisingh9302/knowyourscreen",
-      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/Screenshot_428_fc85ry.png",
+      src: "https://res.cloudinary.com/dgo0fkgwh/image/upload/v1718624869/portfolio/Screenshot_428_fc85ry.png",
       title: "Know Your Screen",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
